093e1b256cb9f03e464add5460769bbcb0c585f8
ezskyyoung <ezskyyoung@51f2dc44-eae3-11de-88d6-afc75ad17f39>
2010-07-27 06:43:32 +0000

    加入繁体中文语言包

---
+++ modules/support/trunk/language/tchinese_utf8/main.php
@@ -0,0 +1,103 @@
+<?php
+// header.php
+define('_MA_SUPPORT_LOGIN',"請先登入！");
+define('_MA_SUPPORT_QUESTIONSUMBIT',"問題提交");
+define('_MA_SUPPORT_READ',"已認領");
+define('_MA_SUPPORT_DOING',"處理中");
+define('_MA_SUPPORT_QUESTIONREPLY',"問題已答復");
+define('_MA_SUPPORT_USERFEEDBACK',"問題補充");
+define('_MA_SUPPORT_QUESTIONRESUMBIT',"問題被再一次提交"); 
+define('_MA_SUPPORT_SUBMITINFO_UNSURE',"使用者已提交了訊息，請您盡快確認。");
+define('_MA_SUPPORT_INFOREAD_DOING',"客服人員已認領了您的消息，正在處理中..."); 
+define('_MA_SUPPORT_INFOACTON_WAITING',"客服人員正在解決您的問題，這可能需要一段時間，請您耐心等候...");
+define('_MA_SUPPORT_USERRESUBMIT',"使用者認為客服的答復不是滿意的答復，希望得到更多的決議程序...");
+define('_MA_SUPPORT_QUESTIONFORWARDOTHER',"問題轉交給其它客服人員處理，請您耐心等候..."); 
+define('_MA_SUPPORT_QUESTIONFINISH',"問題已解決");
+define('_MA_SUPPORT_QUESTIONCLOSE',"問題已被管理員關閉，關閉原因..."); 
+define('_MA_SUPPORT_QUESTIONIGNORE',"問題已被使用者忽略，忽略原因...");
+define('_MA_SUPPORT_TIMEFORMATC',"y-m-d");
+define('_MA_SUPPORT_TIMEFORMATE',"y-m-d h:i:s");
+define('_MA_SUPPORT_TIMEFORMATS',"Y年m月d日");
+define('_MA_SUPPORT_TIMEFORMATL',"Y年m月d日h小時:i分鐘:s秒");
+//category.php
+//action.category.php
+//ajax.php
+//action.php
+define('_MA_SUPPORT_NOPEREXPIRED',"您沒有該問題的動作權限！");
+define('_MA_SUPPORT_QUESTIONREAD',"該問題已被認領！");
+define('_MA_SUPPORT_NOCHOICECAT',"沒有選擇類別別！");
+// index.php
+define('_MA_SUPPORT_SEND',"已傳送");
+//class category.php
+define('_MA_SUPPORT_UPDATECAT',"更新分類");
+define('_MA_SUPPORT_CATLOGO',"題頭圖片");
+define('_MA_SUPPORT_REUPLOADLOGOTIP',"從新上傳將會覆寫現有檔案");
+define('_MA_SUPPORT_UPLOADSTYLE',"允許上傳的類型為jpeg,pjpeg,gif,png檔案");
+define('_MA_SUPPORT_CATDESC',"分類描述");
+define('_MA_SUPPORT_VISIBLE',"是否顯示");
+define('_MA_SUPPORT_MANGER',"客服管理員");
+//class process.php
+define('_MA_SUPPORT_QUESTIONNAME',"問題名稱");
+define('_MA_SUPPORT_QUESTIONDESC',"問題描述");
+define('_MA_SUPPORT_ADDANNEX',"再加入一個附屬應用程式");
+define('_MA_SUPPORT_FORWARDCAT',"轉發類別");
+define('_MA_SUPPORT_FORWORDMANGER',"轉發管理員");
+define('_MA_SUPPORT_NOCHOICEMANGER',"不選擇管理員則所有管理都有認領權！");
+// support_category.html
+define('_MA_SUPPORT_WEIGHT',"排序");
+define('_MA_SUPPORT_EDIT',"編輯");
+// support_index.html
+define('_MA_SUPPORT_TITLE',"標題");
+define('_MA_SUPPORT_UNDONE',"未完成");
+// support_info.html
+define('_MA_SUPPORT_CREATOR',"建立者");
+define('_MA_SUPPORT_QUESTIONCAT',"問題分類");
+define('_MA_SUPPORT_UNREAD',"尚未認領");
+define('_MA_SUPPORT_REPLAY',"回復");
+define('_MA_SUPPORT_MANGER',"客服經理");
+define('_MA_SUPPORT_SUPPORT',"客服");
+define('_MA_SUPPORT_SUBMITTER',"提交者");
+define('_MA_SUPPORT_TIME',"時間:");
+define('_MA_SUPPORT_DOREAD',"認領");
+define('_MA_SUPPORT_FORWARD',"轉發");
+define('_MA_SUPPORT_BACK',"傳回");
+
+//common const
+define("_AM_SUPPORT_SAVEDSUCCESS", "儲存成功");
+define("_AM_SUPPORT_UPDATEDSUCCESS", "更新成功！");
+define("_AM_SUPPORT_DELETEDSUCCESS", "移除成功");
+define("_AM_SUPPORT_ACTIVEDSUCCESS", "動作成功");
+define("_AM_SUPPORT_RUSUREDEL", "確定要移除 [%s]");
+define('_MA_SUPPORT_LATESTPUBLISH',"最新發布");
+define('_MA_SUPPORT_UNREAD',"未認領");
+define('_MA_SUPPORT_TREATED',"已處理");
+define('_MA_SUPPORT_UNTREATED',"未處理");
+define('_MA_SUPPORT_SOLVE',"已解決");
+define('_MA_SUPPORT_BYCAT',"按類別");
+define('_MA_SUPPORT_QUESTIONMANAGEMENT',"問題管理");
+define('_MA_SUPPORT_QUESTIONFORWARD',"問題轉發");
+define('_MA_SUPPORT_CATMANAGEMENT',"分類管理");
+define('_MA_SUPPORT_FORWARDTO',"轉向到");
+define('_MA_SUPPORT_CHOICE',"請選擇");
+define('_MA_SUPPORT_NOQUESTION',"沒有該問題！");
+define('_MA_SUPPORT_NOCAT',"沒有該分類!");
+define('_MA_SUPPORT_DEFAULTCAT',"預設類別");
+define('_MA_SUPPORT_CLAIM',"尚未認領");
+define('_MA_SUPPORT_CATNAME',"分類名稱");
+define('_MA_SUPPORT_ADDCAT',"加入分類");
+define('_MA_SUPPORT_ANNEX',"附屬應用程式");
+define('_MA_SUPPORT_ASK',"我要提問");
+define('_MA_SUPPORT_SEARCH',"搜索");
+define('_MA_SUPPORT_QUERY',"查詢");
+define('_MA_SUPPORT_ACTIVE',"動作");
+define('_MA_SUPPORT_BELONGCAT',"所屬類別");
+define('_MA_SUPPORT_CURRENTSTATUS',"目前狀態");
+define('_MA_SUPPORT_PUBDATE',"發布時間");
+define('_MA_SUPPORT_ENDDATE',"結束時間");
+define('_MA_SUPPORT_QUESTIONREPLY',"問題回復");
+define('_MA_SUPPORT_DONE',"完成");
+define('_MA_SUPPORT_IGNORE',"忽略");
+define('_MA_SUPPORT_CLOSE',"關閉");
+define('_MA_SUPPORT_SAVEERROR',"儲存有誤，請從新填寫,多次不成請洽詢客服人員。");
+
+?>
---
+++ modules/support/trunk/language/tchinese_utf8/modinfo.php
@@ -0,0 +1,11 @@
+<?php
+define('_MI_SUPPORT_NAME',"客服模組");
+define('_MI_SUPPORT_DESC',"客服模組的描述");
+
+define('_MI_SUPPORT_TIME',"網站時間格式選擇");
+define('_MI_SUPPORT_PAGENAV',"內容顯示條目數");
+define('_MI_SUPPORT_SUPPORT_MANAGER',"選擇客服經理群組");
+define('_MI_SUPPORT_SUPPORT',"選擇客服群組");
+
+
+?>
