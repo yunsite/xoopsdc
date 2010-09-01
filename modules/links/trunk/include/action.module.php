aa434f8424166d6ea027b41ec608f85a57cd7ea8
ezskyyoung <ezskyyoung@51f2dc44-eae3-11de-88d6-afc75ad17f39>
2010-09-01 08:47:20 +0000

    修复默认数据中图片不能正常显示

--- modules/links/trunk/include/action.module.php
+++ modules/links/trunk/include/action.module.php
@@ -1,23 +1,4 @@
 <?php
- /**
- * Links
- *
- * You may not change or alter any portion of this comment or credits
- * of supporting developers from this source code or any supporting source code 
- * which is considered copyrighted (c) material of the original comment or credit authors.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * @copyright      The XOOPS Co.Ltd. http://www.xoops.com.cn
- * @license        http://www.fsf.org/copyleft/gpl.html GNU public license
- * @package        links
- * @since          1.0.0
- * @author         Mengjue Shao <magic.shao@gmail.com>
- * @author         Susheng Yang <ezskyyoung@gmail.com>  
- * @version        $Id: action.module.php 1 2010-1-22 ezsky$
- */
-
 if (!defined('XOOPS_ROOT_PATH')){ exit(); }
 
 function xoops_module_install_links(&$module)
@@ -28,10 +9,18 @@ function xoops_module_install_links(&$module)
         $module->setErrors("Pre-set data were not installed");
         return true;
     }
+    
+    $sql  = "INSERT INTO " . $GLOBALS["xoopsDB"]->prefix("links_link") . " (`link_id`, `cat_id`, `link_title`, `link_url`, `link_desc`, `link_order`, `link_status`, `link_image`, `link_dir`, `published`, `datetime`) VALUES";
+    $sql .= "(1, 1, '众锐普斯', 'http://www.xoops.com.cn/', 'XOOPS商业公司', 1, 1, '', '" . XOOPS_URL . "/modules/links/images/xoops.com.png', 1251831615, 1251831615),";
+    $sql .= "(2, 1, 'XOOPS中文社区', 'http://xoops.org.cn/', 'Xoops中文支持站', 2, 1, '', '" . XOOPS_URL . "/modules/links/images/xoops.org.png', 1251831616, 1251831616);";
+    $GLOBALS["xoopsDB"]->queryF($sql);
     return true ;
 }
 function xoops_module_update_links(&$module, $prev_version = null)
 {
+    global $xoopsDB;
+    $query = "ALTER TABLE `".$xoopsDB->prefix("links_link")."`  ADD `link_contact` varchar (255) NOT NULL ;";
+    $xoopsDB->queryF($query);
     return true;
 }
 ?>
--- modules/links/trunk/sql/mysql.links.sql
+++ modules/links/trunk/sql/mysql.links.sql
@@ -1,4 +1,1 @@
 INSERT INTO `links_category` (`cat_id`, `cat_name`, `cat_desc`, `cat_order`, `cat_status`) VALUES (1, '友情链接', '', 1, 0);
-INSERT INTO `links_link` (`link_id`, `cat_id`, `link_title`, `link_url`, `link_desc`, `link_order`, `link_status`, `link_image`, `link_dir`, `published`, `datetime`, `link_contact`) VALUES
-(1, 1, '众锐普斯', 'http://www.xoops.com.cn/', 'XOOPS商业公司', 1, 1, '', 'http://localhost/test/modules/links/images/xoops.com.png', 1251831615, 1251831615, ''),
-(2, 1, 'XOOPS中文社区', 'http://xoops.org.cn/', 'Xoops中文支持站', 2, 1, '', 'http://localhost/test/modules/links/images/xoops.org.png', 1251831616, 1251831616, '');
