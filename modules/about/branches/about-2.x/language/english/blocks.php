5750941b701d456dc327755f8cbff3965c2ee42b
ezskyyoung <ezskyyoung@51f2dc44-eae3-11de-88d6-afc75ad17f39>
2010-09-01 09:07:52 +0000

    init about2.x

---
+++ modules/about/branches/about-2.x/language/english/blocks.php
@@ -0,0 +1,22 @@
+<?php
+ /**
+ * About
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code 
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright      The XOOPS Co.Ltd. http://www.xoops.com.cn
+ * @license        http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package        about
+ * @since          1.0.0
+ * @author         Mengjue Shao <magic.shao@gmail.com>
+ * @author         Susheng Yang <ezskyyoung@gmail.com>
+ * @version        $Id: blocks.php 1 2010-2-9 ezsky$
+ */
+
+
+?>
